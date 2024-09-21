8504e85c63c5ed483a891b6a2e69bb8049ff5e89
Nick Towle <ntowle@gmail.com>
2024-09-21 19:43:39 -0400

    Update itIT (thanks Grifo92)

--- Locales/itIT.lua
+++ Locales/itIT.lua
@@ -1,13 +1,13 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("GreatVaultKeyInfo", "itIT")
 if not L then return end
 
---L.reward_locked = "Reward Locked"
---L.run_to_unlock = "Run %1$d more to unlock"
---L.run_this_week = "%1$d run this week"
---L.runs_this_week = "%1$d runs this week"
---L.top_runs_this_week = "Top %d of %d Runs This Week"
---L.myth = "Myth"
---L.hero = "Hero"
---L.champion = "Champion"
---L.veteran = "Veteran"
---L.adventurer = "Adventurer"
+L.reward_locked = "Ricompensa Bloccata"
+L.run_to_unlock = "Spedizioni %1$d per sbloccare"
+L.run_this_week = "%1$d spedizione questa settimana"
+L.runs_this_week = "%1$d spedizioni questa settimana"
+L.top_runs_this_week = "Migliori %d di %d Spedizioni questa settimana"
+L.myth = "Mito"
+L.hero = "Eroe"
+L.champion = "Campione"
+L.veteran = "Veterano"
+L.adventurer = "Avventuriero"
