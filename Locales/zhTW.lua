f318328bd4437f79e3c225035371326201c6500a
Maple Saint <2303601+nanjuekaien1@users.noreply.github.com>
2024-09-22 20:46:54 +0800

    Update zhTW.lua

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -1,13 +1,13 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("GreatVaultKeyInfo", "zhTW")
 if not L then return end
 
---L.reward_locked = "Reward Locked"
---L.run_to_unlock = "Run %1$d more to unlock"
---L.run_this_week = "%1$d run this week"
---L.runs_this_week = "%1$d runs this week"
---L.top_runs_this_week = "Top %d of %d Runs This Week"
---L.myth = "Myth"
---L.hero = "Hero"
---L.champion = "Champion"
---L.veteran = "Veteran"
---L.adventurer = "Adventurer"
+L.reward_locked = "鎖定奬勵"
+L.run_to_unlock = "完成 %1$d 次可以解鎖"
+L.run_this_week = "本週已完成 %1$d 次"
+L.runs_this_week = "本週共完成 %1$d 次"
+L.top_runs_this_week = "本週共完成 %d 次，其中限時 %d 次。"
+L.myth = "神話"
+L.hero = "英雄"
+L.champion = "勇士"
+L.veteran = "精兵"
+L.adventurer = "冒險者"
